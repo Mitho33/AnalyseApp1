19de3386945036cd5d7f98dc8ae021f4245481cf
Michael Thomas <129625504+Mitho33@users.noreply.github.com>
2025-11-28 22:37:49 +0100

    Update TB11.py

--- TB12/TB11.py
+++ TB12/TB11.py
@@ -398,15 +398,16 @@ class PageFactory:
 st.set_page_config(page_title="Bilanzanalyse", layout="wide")
 #st.sidebar.title("📌 Navigation")
   #mit r werden die Slashes nicht interpretiert, oder \\
-st.sidebar.image("LogoMT.png", width=120)
+#st.sidebar.image("LogoMT.png", width=120)
 
-#st.sidebar.image(
-#    "https://raw.githubusercontent.com/Mitho33/AnalyseApp1/main/TB12/LogoMT.png",
-#    width=120
-#)
+st.sidebar.image(
+     "https://raw.githubusercontent.com/Mitho33/AnalyseApp1/main/TB12/LogoMT.png",
+     width=120
+ )
 
 seiten = list(PageFactory._pages.keys())
 wahl = st.sidebar.radio("Seite auswählen:", seiten)
 
 seite_obj = PageFactory.create(wahl)
 seite_obj.render()
+
